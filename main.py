9bc472f45f873ce8ef36e0d5cc92e84af079c773
glindsona-ux <glindsona@gmail.com>
2026-05-14 02:07:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from threading import Thread
+from flask import Flask
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+
+# Servidor web pra manter online
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 intents = discord.Intents.default()
 intents.members = True
@@ -12,15 +28,4 @@ intents.guilds = True
 intents.invites = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Bot online como {bot.user}")
-
-async def main():
-    await bot.load_extension("bot.convite")
-    await bot.start(TOKEN)
-
-import asyncio
-asyncio.run(main())
+bot = commands.Bot(command_prefix="!", int
